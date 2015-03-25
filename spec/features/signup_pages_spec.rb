e799420029e39c21b254ccb620815cb39315b711
Alex Beeken and Carli Martinez <martinezcarli@gmail.com>
2015-03-25 15:11:26 -0700

    test signup process with capybara and factory girl

--- spec/features/signup_pages_spec.rb
+++ spec/features/signup_pages_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 describe "signing up" do
   it "signs a user up when provided correct information" do
-  visit '/'
-  click_on 'Sign up'
-  fill_in 'Email', :with => 'butterscotch@yahoo.com'
-  fill_in 'Password', :with => 'mushroompizza'
-  fill_in 'Password confirmation', :with => 'mushroompizza'
-  click_button 'Sign up'
-  expect(page).to have_content "Logged in as butterscotch@yahoo.com"
+    visit '/'
+    click_on 'Sign up'
+    fill_in 'Email', :with => 'butterscotch@yahoo.com'
+    fill_in 'Password', :with => 'mushroompizza'
+    fill_in 'Password confirmation', :with => 'mushroompizza'
+    click_button 'Sign up'
+    expect(page).to have_content "Logged in as butterscotch@yahoo.com"
+  end
+
+  it "logs a user in when provided correct information" do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on 'Login'
+    fill_in 'Email', :with => 'me@me.com'
+    fill_in 'Password', :with => '12345678'
+    click_button 'Log in'
+    expect(page).to have_content "Logged in as me@me.com"
   end
 end
