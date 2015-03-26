5082468d6712531b1e7c1c43d33152f516cb9b23
Alex Beeken and Carli Martinez <student@epicodus.com>
2015-03-26 11:38:41 -0700

    add room form and change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
 
-  resource :rooms do
-    resource :reservations
-  end
+  resource :rooms 
+
+  resource :reservations
+
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 end
